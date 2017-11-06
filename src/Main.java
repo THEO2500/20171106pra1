22f2de12f8a8f11baa16c1e3529f552951e93f68
THEO2500 <worldpeace871219@gmail.com>
2017-11-06 18:51:46 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=0;
+	while (a <= 0)
+		{
+			System.out.println(a%2==0?"*****" : "+++++");
+			a++ ;
+		}
+
+
+
+
+
+
+    }
+}
